3496da666b9cc4846acf64d12f5d860e44e1b399
Vaibhav <dev.vaibhav99@gmail.com>
2023-10-09 11:27:53 +0530

    todos now displayed using a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -12,7 +13,6 @@ const App: React.FC = () => {
     if (todo) {
       setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
       setTodo("");
-      console.log(todos);
     }
   };
 
@@ -20,9 +20,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      {todos.map((t) => (
-        <li>{t.todo}</li>
-      ))}
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
